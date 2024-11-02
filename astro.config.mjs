423c5a5850ac3dec34e6e7f5b8f0a42cc9f1f3d3
David Berg <david.nossebro@gmail.com>
2024-11-02 16:02:38 +0100

    Added sidebar and worked with layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: netlify({
     edgeMiddleware: true
-  })
+  }),
+
+  integrations: [
+    tailwind(),
+  ]
 });
